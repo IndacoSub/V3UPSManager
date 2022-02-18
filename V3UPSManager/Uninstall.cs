d1ca4afaef25370b9a334629c1151803d8b5e682
IndacoSub <67112008+IndacoSub@users.noreply.github.com>
2022-02-18 23:17:31 +0100

    Add initial support for the Unity-based port(s) (WIP, untested)

---
+++ V3UPSManager/Uninstall.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace V3UPSManager
+{
+    public partial class Form1 : Form
+    {
+        private void Uninstall()
+        {
+            // Check if the installation folder is not valid
+            if (string.IsNullOrWhiteSpace(verified_installation_folder) || verified_installation_folder.Length <= 0 || !Directory.Exists(verified_installation_folder))
+            {
+                DisplayInfo.Print(info[25]);
+                return;
+            }
+
+            List<string> filelist = new List<string>();
+            // Count and get all .spc backup files in the patch folder
+            string[] files1 = Directory.GetFiles(verified_installation_folder, ("*.spc_bak"), SearchOption.AllDirectories);
+            string[] files2 = Directory.GetFiles(verified_installation_folder, ("*.SPC_bak"), SearchOption.AllDirectories);
+            if (files1 == null || files2 == null || files1.Length == 0 || files2.Length == 0)
+            {
+                DisplayInfo.Print(info[32]);
+                return;
+            }
+
+            foreach (string file in files1)
+            {
+                filelist.Add(file);
+            }
+
+            foreach (string file in files2)
+            {
+                filelist.Add(file);
+            }
+
+            int count_uninstalled = 0;
+            foreach (string file in filelist)
+            {
+                // .spc_bak
+                string actual_file = file;
+                // .spc
+                string normal_file = actual_file.Substring(0, actual_file.Length - 4); // 4 is "_bak"'s length
+                if (!File.Exists(normal_file) || string.IsNullOrWhiteSpace(normal_file))
+                {
+                    continue;
+                }
+
+                if (!File.Exists(actual_file) || !File.Exists(file))
+                {
+                    continue;
+                }
+
+                // Delete the "normal" file
+                File.Delete(normal_file);
+
+                // Copy the bak file to the "normal" file (a.k.a. restore backup)
+                File.Move(actual_file, normal_file, true);
+
+                // If for some reason it still exists even after being moved
+                if (File.Exists(actual_file))
+                {
+                    // Delete the .bak file
+                    File.Delete(actual_file);
+                }
+
+                //DisplayInfo.Print("Normal: " + normal_file + ", actual: " + actual_file + "...");
+                count_uninstalled++;
+            }
+
+            if (count_uninstalled > 0)
+            {
+                DisplayInfo.Print(info[33]);
+            }
+            else
+            {
+                DisplayInfo.Print(info[32]);
+            }
+        }
+    }
+}
---
+++ V3UPSManager/Utils.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace V3UPSManager
+{
+    public partial class Form1 : Form
+    {
+        // From https://stackoverflow.com/questions/16183788/case-sensitive-directory-exists-file-exists
+        // (edited)
+        static public bool DirExistsMatchCase(string path)
+        {
+
+            // Figure out if the case (of the final part) is the same
+            string thisDir = Path.GetFileName(path);
+            thisDir = thisDir.Substring(thisDir.LastIndexOf('\\') + 1);
+            string actualDir = Path.GetFileName(Directory.GetDirectories(Path.GetDirectoryName(path), thisDir)[0]);
+            //DisplayInfo.Print("thisDir: " + thisDir + ", actualDir: " + actualDir);
+            return thisDir == actualDir;
+        }
+
+        // From https://stackoverflow.com/questions/16183788/case-sensitive-directory-exists-file-exists
+        public static bool FileExistsCaseSensitive(string filename)
+        {
+            string name = Path.GetDirectoryName(filename);
+
+            return name != null
+                   && Array.Exists(Directory.GetFiles(name), s => s == Path.GetFullPath(filename));
+        }
+
+        // From https://stackoverflow.com/questions/2435695/converting-a-md5-hash-byte-array-to-a-string
+        public static string ToHex(byte[] bytes, bool upperCase)
+        {
+            StringBuilder result = new StringBuilder(bytes.Length * 2);
+
+            for (int i = 0; i < bytes.Length; i++)
+                result.Append(bytes[i].ToString(upperCase ? "X2" : "x2"));
+
+            return result.ToString();
+        }
+
+        // From https://stackoverflow.com/questions/1395205/better-way-to-check-if-a-path-is-a-file-or-a-directory
+
+        public static bool IsDirectory(string path)
+        {
+            if(!File.Exists(path) && !Directory.Exists(path))
+            {
+                return false;
+            }
+
+            // get the file attributes for file or directory
+            FileAttributes attr = File.GetAttributes(path);
+
+            if (attr.HasFlag(FileAttributes.Directory))
+                return true;
+            else
+                return false;
+        }
+    }
+}
