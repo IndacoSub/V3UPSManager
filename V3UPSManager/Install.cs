56558253e5f8e0056f79f2fee7aafda94a1cf56f
IndacoSub <67112008+IndacoSub@users.noreply.github.com>
2024-10-28 15:40:00 +0100

    Fix errors due to sorting

--- V3UPSManager/Install.cs
+++ V3UPSManager/Install.cs
@@ -66,8 +66,19 @@ public partial class MainWindow : Form
 			return;
 		}
 
+		// DO NOT SORT THE STRING ARRAYS
+		/*
+		string logFileApplyBefore = "to_apply_before.txt";
+		string logFileApplyAfter = "to_apply_after.txt";
+		string logFileToBeAppliedBefore = "to_be_applied_before.txt";
+		string logFileToBeAppliedAfter = "to_be_applied_after.txt";
+		File.WriteAllLines(logFileApplyBefore, to_apply);
+		File.WriteAllLines(logFileToBeAppliedBefore, to_be_applied);
 		to_apply.Sort();
 		to_be_applied.Sort();
+		File.WriteAllLines(logFileApplyAfter, to_apply);
+		File.WriteAllLines(logFileToBeAppliedAfter, to_be_applied);
+		*/
 
 		// *If they don't exist already*, copy the SPCs/Unity-files
 		// as .*_bak files (backup)
@@ -79,26 +90,42 @@ public partial class MainWindow : Form
 		// As of June 2023, the latest game version for the Legacy (Steam) version is 1.01
 		// It's safe to assume that there won't be any update anytime soon
 
+		// Foreach in to_apply (patch files?)
 		foreach (string file in to_apply)
 		{
+			// Ignore bak files in to_apply
 			if (to_apply.Contains("_bak"))
 			{
 				continue;
 			}
 
+			// If ex. game_resident_US.spc_bak DOES NOT exist while game_resident_US.spc DOES exist
 			if (!File.Exists(file + "_bak") && File.Exists(file))
 			{
-				// Only create one when there isn't one
+				// Make a backup of the .spc file
+				// Only create a backup when there isn't one
 				File.Copy(file, file + "_bak", false);
 			}
 
+			// If ex. game_resident_US.spc DOES exist while game_resident_US.spc_bak ALSO DOES exist: 
 			if (File.Exists(file) && File.Exists(file + "_bak"))
 			{
+				// Delete game_resident_US.spc
 				File.Delete(file);
 			}
+
+			// October 2024:
+			// Copy the _bak to the file
+			File.Copy(file + "_bak", file, true);
 		}
 
 		// Apply the patches
+
+		string logFile = "patch_log.txt";
+		if (File.Exists(logFile))
+		{
+			File.Delete(logFile);
+		}
 
 		for (int j = 0; j < to_apply.Count(); j++)
 		{
@@ -145,19 +172,42 @@ public partial class MainWindow : Form
 			//Process.Start(ups, "apply --base " + $"\"{before}\"" + " --patch " + $"\"{after}\"" + " --output " + $"\"{outfile}\"").WaitForExit();
 
 			var p = new Process();
-			//p.StartInfo.WorkingDirectory = cur;
 			p.StartInfo.FileName = patch_tool_exe;
 			p.StartInfo.Arguments = command;
-			p.StartInfo.RedirectStandardOutput = false;
+			p.StartInfo.RedirectStandardOutput = true;
+			p.StartInfo.RedirectStandardError = true;
 			p.StartInfo.UseShellExecute = false;
 			p.StartInfo.CreateNoWindow = true;
+
+			if (false)
+			{
+				DisplayInfo.Print(patch_tool_exe + " used: " + command);
+				Clipboard.SetText(patch_tool_exe + " " + command);
+			}
+			File.AppendAllText(logFile, patch_tool_exe + " " + command + Environment.NewLine);
+
 			p.Start();
+
+			string output = p.StandardOutput.ReadToEnd();
+			string error = p.StandardError.ReadToEnd();
+
 			p.WaitForExit();
 
+			if (output.Length > 0)
+			{
+				//DisplayInfo.Print("Output:\n" + output);
+			}
+
+			if (error.Length > 0)
+			{
+				DisplayInfo.Print("Error:\n" + error);
+			}
+
+			// WHY IS THIS AFTER INSTALLING? (October 2024)
 			// If the outfile (not _bak) file does not exist while the backup does
 			if (!File.Exists(outfile) && File.Exists(before))
 			{
-				DisplayInfo.Print("File does not exist: " + outfile +
+				DisplayInfo.Print("POST-INSTALL: File does not exist: " + outfile +
 								  ", creating manually from backup... (please report this)");
 				// Then copy the backup
 				File.Copy(before, outfile, false);
